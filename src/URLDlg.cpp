b5dd86d08c4efce9b3e73b7e8268401d28969765
tortoisesvn <tortoisesvn@6a09d29b-0d3f-0410-9acd-1f053ec18787>
2009-09-10 18:38:08 +0000

    Fix tooltip example.

--- src/URLDlg.cpp
+++ src/URLDlg.cpp
@@ -54,7 +54,7 @@ LRESULT CURLDlg::DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			AddToolTip(IDC_URLTOMONITOR, _T("URL to the repository, or the SVNParentPath URL"));
 			AddToolTip(IDC_IGNORESELF, _T("If enabled, commits from you won't show a notification"));
 			AddToolTip(IDC_IGNOREUSERS, _T("enter a list of usernames to ignore, separated by newlines"));
-			AddToolTip(IDC_SCRIPT, _T("enter here a command which gets called after new revisions were detected.\n\n%revision gets replaced with the new HEAD revision\n%url gets replaced with the url of the project\n%project gets replaced with the project name\n\nExample command line:\nTortoiseProc.exe /command:update /rev:%revision path\\to\\working\\copy"));
+			AddToolTip(IDC_SCRIPT, _T("enter here a command which gets called after new revisions were detected.\n\n%revision gets replaced with the new HEAD revision\n%url gets replaced with the url of the project\n%project gets replaced with the project name\n\nExample command line:\nTortoiseProc.exe /command:update /rev:%revision /path:\"path\\to\\working\\copy\""));
 			AddToolTip(IDC_WEBDIFF, _T("URL to a web viewer\n%revision gets replaced with the new HEAD revision\n%url gets replaced with the url of the project\n%project gets replaced with the project name"));
 			if (info.minminutesinterval)
 			{
