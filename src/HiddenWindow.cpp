07304ae774e56cab27498bdc8dc44e89a6e931b9
tortoisesvn <tortoisesvn@6a09d29b-0d3f-0410-9acd-1f053ec18787>
2010-10-09 13:39:27 +0000

    A click on the tray icon closes the main window if it's visible.
    Update issue #281

--- src/HiddenWindow.cpp
+++ src/HiddenWindow.cpp
@@ -145,8 +145,13 @@ LRESULT CHiddenWindow::HandleCustomMessages(HWND /*hwnd*/, UINT uMsg, WPARAM wPa
             // bring the dialog to front
             if (m_hMainDlg == NULL)
                 m_hMainDlg = FindWindow(NULL, _T("Commit Monitor"));
-            SetWindowPos(m_hMainDlg, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE|SWP_NOSIZE|SWP_SHOWWINDOW);
-            SetForegroundWindow(m_hMainDlg);
+            if (IsWindowVisible(m_hMainDlg))
+                SendMessage(m_hMainDlg, WM_CLOSE, 0, 0);
+            else
+            {
+                SetWindowPos(m_hMainDlg, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE|SWP_NOSIZE|SWP_SHOWWINDOW);
+                SetForegroundWindow(m_hMainDlg);
+            }
             return TRUE;
         }
         m_bMainDlgShown = true;
