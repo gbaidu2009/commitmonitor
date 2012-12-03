0a089fba4f28b5c120f37b8a124f6c0a7a30f174
XhmikosR <XhmikosR@6a09d29b-0d3f-0410-9acd-1f053ec18787>
2012-12-03 11:46:29 +0000

    whitespace changes

--- src/CommitMonitor.cpp
+++ src/CommitMonitor.cpp
@@ -40,9 +40,9 @@ HANDLE g_mutex = 0;
 #define APPNAME_MUTEX _T("CommitMonitor_{3802F59C-BEBD-49b9-A345-F99CBA2FBD0D}")
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
--- src/FindBar.h
+++ src/FindBar.h
@@ -1,6 +1,6 @@
 // CommitMonitor - simple checker for new commits in svn repositories
 
-// Copyright (C) 2007 - Stefan Kueng
+// Copyright (C) 2007, 2012 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -31,7 +31,7 @@ public:
     CFindBar();
     ~CFindBar(void);
 
-    void                    SetParent(HWND hParent) {m_hParent = hParent;}
+    void                    SetParent(HWND hParent) { m_hParent = hParent; }
 protected:
     LRESULT CALLBACK        DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
     LRESULT                 DoCommand(int id, int msg);
