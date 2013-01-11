087162b7c01db83769e87e5c3bb4b2303e6c5439
tortoisesvn <tortoisesvn@6a09d29b-0d3f-0410-9acd-1f053ec18787>
2013-01-11 19:35:23 +0000

    Check that VerQueryValue returns a useful value.

--- src/AppUtils.cpp
+++ src/AppUtils.cpp
@@ -1,6 +1,6 @@
 // CommitMonitor - simple checker for new commits in svn repositories
 
-// Copyright (C) 2007-2010, 2012 - Stefan Kueng
+// Copyright (C) 2007-2010, 2012-2013 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -466,8 +466,8 @@ std::wstring CAppUtils::GetVersionStringFromExe(LPCTSTR path)
                 (LPTSTR)strLangProductVersion,
                 (LPVOID *)&lpVersion,
                 &nInfoSize);
-
-            sVersion = (LPCTSTR)lpVersion;
+            if (nInfoSize && lpVersion)
+                sVersion = (LPCTSTR)lpVersion;
             free(pBuffer);
         }
     }
